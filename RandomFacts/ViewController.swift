c96b19bc58a743f5f9065114657fe8daea3ee759
Azhar Islam <azhar.islam@ynap.com>
2019-11-09 17:51:56 +0000

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Azhar Islam on 09/11/2019.
+//  Copyright © 2019 Azhar Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
