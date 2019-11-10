4fbd376d12a1ee2273084a463e82749be96b9e72
Azhar Islam <azhar.islam@ynap.com>
2019-11-10 01:28:06 +0000

    created ui

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var factButton: UIButton!
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showFactButtonTapped(_ sender: Any) {
+        factLabel.text = "This is a fun fact"
+    }
+    
 
 
 }
